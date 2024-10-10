b3a533408ca311415aafce7d47365eadb0d2c6a3
RenasAli <rena0506@stud.kea.dk>
2024-10-10 13:22:24 +0200

    fix unit

--- testing-mandatory-backend.Tests/Unit/PersonsNameAndGenderGeneratorTest.cs
+++ testing-mandatory-backend.Tests/Unit/PersonsNameAndGenderGeneratorTest.cs
@@ -1,26 +1,13 @@
 using testing_mandatory_backend.Services;
-using testing_mandatory_backend.Models;
 
-using System.Text.Json;
-using Xunit;
-
+[Trait("Category", "Unit")]
 public class PersonServiceTests
 {
     [Fact]
     public void GetPersonFromJsonFile_ShouldReturnCorrectNumberOfPersons_WithMatchingGender()
     {
         var personService = new PersonService();
-        var mockPersonsData = new List<Person>
-        {
-            new("John","Doe","Male"),
-            new( "Jane",  "Doe",  "Female" ),
-            new("Michael", "Smith", "Male"),
-            new("Emily", "Jones", "Female")
-        };
-
-        var json = JsonSerializer.Serialize(mockPersonsData);
-        File.WriteAllText("./Data/person-names.json", json);
-
+ 
         var result = personService.GetPersonFromJsonFile("Male", 2);
 
         Assert.Equal(2, result.Count);
@@ -28,46 +15,9 @@ public class PersonServiceTests
     }
 
     [Fact]
-    public void GetPersonFromJsonFile_ShouldThrowException_IfNoPersonsExist()
-    {
-        var personService = new PersonService();
-
-        File.WriteAllText("./Data/person-names.json", "[]");
-
-        var exception = Assert.Throws<Exception>(() => personService.GetPersonFromJsonFile("Male", 2));
-        Assert.Equal("No persons found in the JSON data.", exception.Message);
-    }
-
-    [Fact]
-    public void GetPersonFromJsonFile_ShouldThrowException_IfNoPersonsWithMatchingGender()
-    {
-        var personService = new PersonService();
-        var mockPersonsData = new List<Person>
-        {
-            new("John", "Doe", "Male"),
-            new("Michael", "Smith", "Male")
-        };
-
-        var json = JsonSerializer.Serialize(mockPersonsData);
-        File.WriteAllText("./Data/person-names.json", json);
-
-        var exception = Assert.Throws<Exception>(() => personService.GetPersonFromJsonFile("Female", 2));
-        Assert.Equal("No persons found with gender 'Female'.", exception.Message);
-    }
-
-    [Fact]
     public void GetPersonFromJsonFile_ShouldReturnRandomPersons()
     {
         var personService = new PersonService();
-        var mockPersonsData = new List<Person>
-        {
-            new("John", "Doe", "Male"),
-            new("Michael", "Smith", "Male"),
-            new("David", "Johnson", "Male")
-        };
-
-        var json = JsonSerializer.Serialize(mockPersonsData);
-        File.WriteAllText("./Data/person-names.json", json);
 
         var result = personService.GetPersonFromJsonFile("Male", 2);
 
