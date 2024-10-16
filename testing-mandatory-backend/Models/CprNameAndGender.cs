8cf44a7b56c457bc0a0aa374e081189893be676e
masp1234 <mart2c04@stud.kea.dk>
2024-10-16 17:24:13 +0200

    add interfaces and some models

---
+++ testing-mandatory-backend/Models/CprNameAndGender.cs
@@ -0,0 +1,21 @@
+﻿using Org.BouncyCastle.Asn1.Mozilla;
+
+namespace testing_mandatory_backend.Models
+{
+    public class CprNameAndGender
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Gender { get; set; }
+
+        public string CPR { get; set; }
+        public CprNameAndGender(string name, string surname, string gender, string cpr)
+        {
+            CPR = cpr;
+            Name = name;
+            Surname = surname;
+            Gender = gender;
+
+        }
+    }
+}
---
+++ testing-mandatory-backend/Models/CprNameGenderAndBirthDate.cs
@@ -0,0 +1,23 @@
+﻿using Org.BouncyCastle.Asn1.Mozilla;
+
+namespace testing_mandatory_backend.Models
+{
+    public class CprNameGenderAndBirthDate
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Gender { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public string CPR { get; set; }
+        public CprNameGenderAndBirthDate(string name, string surname, string gender, DateTime birthDate, string cpr)
+        {
+            CPR = cpr;
+            Name = name;
+            Surname = surname;
+            Gender = gender;
+            BirthDate = birthDate;
+
+        }
+    }
+}
