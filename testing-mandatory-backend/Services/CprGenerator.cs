5d10175baa83fb9fea5c2097396fbbf922b9a33f
August Dedenroth <augu0577@stud.kea.dk>
2024-10-16 12:08:13 +0200

    CPR that also returns birthday and person/gender

--- testing-mandatory-backend/Services/CprGenerator.cs
+++ testing-mandatory-backend/Services/CprGenerator.cs
@@ -1,11 +1,11 @@
-namespace testing_mandatory_backend.Services
-{
-    public class CprGenerator
-    {
+using testing_mandatory_backend.Repositories;
+using testing_mandatory_backend.Models;
+
+namespace testing_mandatory_backend.Services {
+    public class CprGenerator {
         private static readonly Random random = new Random();
 
-        public string GenerateRandomCpr()
-        {
+        public string GenerateRandomCpr() {
             // Generates a random date that's between 18 and 100 years ago
             DateTime startDate = DateTime.Today.AddYears(-100);
             DateTime endDate = DateTime.Today.AddYears(-18);
@@ -22,9 +22,41 @@ namespace testing_mandatory_backend.Services
             return datePart + randomDigits;
         }
 
-        public string GenerateCprWithBirthdayAndGender() {
+        public (string cpr, Person randomPerson, DateTime randomBirthday) GenerateCprWithBirthdayAndGender(string gender) {
+            // Generate a random birthday
+            DateTime randomBirthday = BirthdayGenerator.GenerateRandomBirthday();
 
-            return "test";
+            // Create a new instance of the NameAndGenderGenerator class
+            NameAndGenderGenerator nameAndGenderGenerator = new NameAndGenderGenerator(new NameAndGenderRepository());
+
+            // Get a random person with the specified gender
+            Person randomPerson = nameAndGenderGenerator.GetNameAndGenderFromJsonFile(gender);
+
+            // Format the date-part of the CPR
+            string birthdayPart = randomBirthday.ToString("ddMMyy");
+
+            Random random = new Random();
+            int sequenceNumber = random.Next(0, 5000); // Generate a random sequence number in this range
+
+            // Ensures the last digit is odd for males and even for females
+            int lastDigit = random.Next(0, 10);
+            if (gender.Equals("male", StringComparison.OrdinalIgnoreCase)) {
+                if (lastDigit % 2 == 0)
+                {
+                    lastDigit++;
+                }
+            }
+            else if (gender.Equals("female", StringComparison.OrdinalIgnoreCase)) {
+                if (lastDigit % 2 != 0)
+                {
+                    lastDigit--;
+                }
+            }
+
+            // Combine the parts to form the CPR number
+            string cpr = $"{birthdayPart}-{sequenceNumber:D4}{lastDigit}";
+
+            return (cpr, randomPerson, randomBirthday);
         }
     }
 }
